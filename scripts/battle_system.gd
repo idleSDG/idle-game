38b64b33f62c701655aa32eb64e753f0f333d086
Menuoo <lietuvis7080@gmail.com>
2026-03-11 12:01:01 +0200

    lots of progress

--- scripts/battle_system.gd
+++ scripts/battle_system.gd
@@ -10,21 +10,27 @@ var characterSpawnPos = [$"Main Scene/Control", $"Main Scene/Control2", $"Main S
 var character_scene = load("res://scenes/character.tscn")
 var character_class = load("res://scripts/character.gd")
 
+#var playerCharacter
+var targetIndex = 1
 var characterList = []
+var remainingEnemiesList = []
 var timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	characterList.resize(6)
 	
 	# TEMPORARY CODE
 	var instance = character_scene.instantiate()
 	instance.SetStats(GlobalVariables.GetPlayer())
-	characterList.append(instance)
+	characterList[0] = instance
 	characterSpawnPos[0].add_child(instance)
 	
-	var instance2 = character_scene.instantiate()
-	characterList.append(instance2)
-	characterSpawnPos[1].add_child(instance2)
+	for i in 10:
+		var instance2 = character_scene.instantiate()
+		remainingEnemiesList.append(instance2)
+		#characterList.append(instance2)
+		#characterSpawnPos[1].add_child(instance2)
 	
 	pass # Replace with function body.
 
@@ -33,8 +39,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_visuals()
+	check_death()
 	if timer <= 0:
-		update_visuals()
+		check_enemies()
 		if !try_skills():
 			pass_time(delta)
 		else:
@@ -47,6 +55,28 @@ func _process(delta: float) -> void:
 	pass
 
 
+func check_death():
+	for i in range(0, 6):
+		if characterList[i] != null && characterList[i].currentStats.health <= 0:
+			characterList[i].queue_free()
+			characterList[i] = null
+			print("man im dead")
+			
+			for j in range(1, 6):
+				if characterList[j] != null:
+					targetIndex = j
+
+func check_enemies():
+	for i in range(1, 6):
+		if remainingEnemiesList.size() > 0:
+			if characterList[i] == null:
+				characterList[i] = remainingEnemiesList[0]
+				characterSpawnPos[i].add_child(characterList[i])
+				remainingEnemiesList.pop_front()
+		else: break
+	
+	pass
+
 
 func try_skills() -> bool:
 	var i = 0
@@ -54,14 +84,15 @@ func try_skills() -> bool:
 	var maxOvercharge = -1
 	
 	for c in characterList:
-		var overcharge = c.CheckSkillCharge()
-		if overcharge > maxOvercharge:
-			maxOvercharge = overcharge
-			maxPos = i
-		i += 1
+		if c != null:
+			var overcharge = c.CheckSkillCharge()
+			if overcharge > maxOvercharge:
+				maxOvercharge = overcharge
+				maxPos = i
+			i += 1
 	
 	if maxPos != -1:
-		var target = characterList[1] if maxPos == 0 else characterList[0]
+		var target = characterList[targetIndex] if maxPos == 0 else characterList[0]
 		characterList[maxPos].UseSkill(target)
 		return true
 	
@@ -70,10 +101,12 @@ func try_skills() -> bool:
 
 func pass_time(delta: float) -> void:
 	for c in characterList:
-		c.PassTime(delta)
+		if c != null:
+			c.PassTime(delta)
 	pass
 
 func update_visuals() :
 	for c in characterList:
-		c.UpdateVisuals()
+		if c != null:
+			c.UpdateVisuals()
 	pass
--- scripts/global_variables.gd
+++ scripts/global_variables.gd
@@ -3,7 +3,7 @@ class_name GlobalVariables extends Object
 var playerDefaults
 
 static func GetPlayer() -> CharacterStats:
-	return CharacterStats.Create(100, 50, 100, 0.1, 1.0, 2.0, [], [], [], [])
+	return CharacterStats.Create(200, 50, 100, 0.5, 1.1, 5.0, [], [], [], [])
 	#[0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
 	#[0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
 	#[0.1, 0.1, 0.1, 0.1, 0.1],
