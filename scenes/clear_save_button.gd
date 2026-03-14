604429d73defe9a837ed2561bc58c582c7ed214d
shush2u <siusa.arnas@gmail.com>
2026-03-14 21:06:58 +0200

    feat: implemented Save Manager and moved saving logic out of player inventory code

--- scenes/clear_save_button.gd
+++ scenes/clear_save_button.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_pressed() -> void:
-	PlayerInventory.clear_save()
+	SaveManager.clear_save()
---
+++ scripts/autoloads/save_manager.gd
@@ -0,0 +1,61 @@
+extends Node
+
+const SAVE_PATH = "user://spellcraft-idle-save.json"
+
+func _ready():
+	if save_file_exists():
+		load_game()
+	else:
+		print("No save file found. Creating new save.")
+		init_new_save()
+
+func save_game():
+	var save_data = {
+		"inventory": PlayerInventory.get_save_data(),
+		"timestamp": Time.get_unix_time_from_system()
+	}
+	
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		var json_string = JSON.stringify(save_data, "\t")
+		file.store_string(json_string)
+		file.close()
+		print("Game Saved.")
+
+func init_new_save():
+	PlayerInventory.init_new_save()
+
+func save_file_exists() -> bool:
+	print(FileAccess.file_exists(SAVE_PATH))
+	return FileAccess.file_exists(SAVE_PATH)
+
+func load_game():
+	print("Loading game.")
+	if not save_file_exists():
+		print("No save file found.")
+		return
+		
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	var content = file.get_as_text()
+	file.close()
+	
+	var data = JSON.parse_string(content)
+	if data == null: return
+
+	PlayerInventory.last_inventory_update_unix_time = data.get("timestamp", Time.get_unix_time_from_system())
+	if data.has("inventory"):
+		PlayerInventory.load_save_data(data["inventory"])
+
+	print("Game Loaded.")
+	
+func clear_save() -> void:
+	if FileAccess.file_exists(SAVE_PATH):
+		var dir = DirAccess.open("user://")
+		dir.remove(SAVE_PATH)
+	init_new_save()
+
+func _notification(what):
+	if what == NOTIFICATION_APPLICATION_FOCUS_OUT or what == NOTIFICATION_APPLICATION_PAUSED or what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_game()
+	if what == NOTIFICATION_APPLICATION_FOCUS_IN:
+		load_game()
