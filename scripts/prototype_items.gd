7ae36b2a82e2c4dd52bf66a095036cd01e1ccbb6
Juozas <116970223+Juozis@users.noreply.github.com>
2026-03-18 15:15:50 +0200

    Increase bonuses for easy testing

--- scripts/prototype_items.gd
+++ scripts/prototype_items.gd
@@ -4,16 +4,16 @@ static func get_test_items() -> Array:
 	var staff = EquipmentItem.new()
 	staff.item_name = "Novice Staff"
 	staff.slot = EquipmentItem.Slot.WEAPON
-	staff.damage_bonus_pct = 0.10
+	staff.damage_bonus_pct = 0.8
 
 	var belt = EquipmentItem.new()
 	belt.item_name = "Gatherer's Belt"
 	belt.slot = EquipmentItem.Slot.BELT
-	belt.ingredient_gain_pct = 0.05
+	belt.ingredient_gain_pct = 1.5
 
 	var hat = EquipmentItem.new()
 	hat.item_name = "Wizard's Hat"
 	hat.slot = EquipmentItem.Slot.HAT
-	hat.crit_rate_bonus = 0.02
+	hat.crit_rate_bonus = 0.5
 
 	return [staff, belt, hat]
