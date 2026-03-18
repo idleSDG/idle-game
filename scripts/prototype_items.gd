23b6ef94f3c2ad4b0aaff341561764f4e0a8b191
Juozas <116970223+Juozis@users.noreply.github.com>
2026-03-18 11:47:19 +0200

    Add equipment class and prototype items

---
+++ scripts/prototype_items.gd
@@ -0,0 +1,19 @@
+extends Node
+
+static func get_test_items() -> Array:
+	var staff = EquipmentItem.new()
+	staff.item_name = "Novice Staff"
+	staff.slot = EquipmentItem.Slot.WEAPON
+	staff.damage_bonus_pct = 0.10
+
+	var belt = EquipmentItem.new()
+	belt.item_name = "Gatherer's Belt"
+	belt.slot = EquipmentItem.Slot.BELT
+	belt.ingredient_gain_pct = 0.05
+
+	var hat = EquipmentItem.new()
+	hat.item_name = "Wizard's Hat"
+	hat.slot = EquipmentItem.Slot.HAT
+	hat.crit_rate_bonus = 0.02
+
+	return [staff, belt, hat]
