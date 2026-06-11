82269fe8dfe418d514cd125f7cb6e398c4a09307
shush2u <siusa.arnas@gmail.com>
2026-06-11 16:22:09 +0300

    chore: small refactor and ui improvements for MissingSleepDataPopup

--- src/ui/missing_sleep_data_popup/missing_sleep_data_pop_up.gd
+++ src/ui/missing_sleep_data_popup/missing_sleep_data_pop_up.gd
@@ -2,21 +2,20 @@ extends Control
 
 var sleep: sleep_progress
 
-
 func _ready() -> void:
 	sleep = PlayerInventory.sleep
 	sleep.sleep_data_popup.connect(_on_sleep_popup)
 
 
 func _on_sleep_popup() -> void:
-	%MissingSleepDataPopUp.visible = true
+	self.visible = true
 
 
 func _on_sleep_data_fallback_button_pressed() -> void:
 	sleep.set_fallback_data()
-	%MissingSleepDataPopUp.visible = false
+	self.visible = false
 
 
 func _on_sleep_data_device_data_button_pressed() -> void:
 	sleep.mark_sleep_ready()
-	%MissingSleepDataPopUp.visible = false
+	self.visible = false
